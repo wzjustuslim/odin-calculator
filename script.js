c5a26917273b790c1bc9ef4d903adf130d052989
Justus Lim <70901244+wzjustuslim@users.noreply.github.com>
2024-04-18 14:42:11 +0800

    Add keydown handlers and listeners

--- script.js
+++ script.js
@@ -44,90 +44,101 @@ const cache = []
 const memory = []
 const lastEval = []
 const display = document.getElementsByClassName('display')[0]
-const operations = ['+', '-', '*', '/']
+const operators = ['+', '-', '*', '/']
+const integers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+function handleClear() {
+  cache.length = 0
+  memory.length = 0
+  lastEval.length = 0
+  setDisplay()
+}
 
 Array.from(document.getElementsByClassName('clear')).forEach((el) => {
   el.addEventListener('click', () => {
-    cache.length = 0
-    memory.length = 0
-    lastEval.length = 0
-    setDisplay()
+    handleClear()
   })
 })
+
+function handleDot(dot) {
+  if (!cache.length) cache.push('0')
+  if (cache.includes(dot)) return
+  cache.push(dot)
+  setDisplay(cache.join(''))
+}
 
 Array.from(document.getElementsByClassName('dot')).forEach((el) => {
   el.addEventListener('click', () => {
     const dot = el.textContent
-    if (!cache.length) cache.push('0')
-    if (cache.includes(dot)) return
-    cache.push(dot)
-    setDisplay(cache.join(''))
+    handleDot(dot)
   })
 })
+
+function handleNumbers(number) {
+  cache.push(number)
+  if (cache[0] === '0' && cache[1] === number) cache.shift()
+  setDisplay(cache.join(''))
+}
 
 Array.from(document.getElementsByClassName('number')).forEach((el) => {
   el.addEventListener('click', () => {
     const number = el.textContent
-    cache.push(number)
-    if (cache[0] === '0' && cache[1] === number) cache.shift()
-    setDisplay(cache.join(''))
+    handleNumbers(number)
   })
 })
+
+function handleOperators(operator) {
+  if (cache.length) {
+    memory.push(cache.join(''))
+    cache.length = 0
+  }
+
+  if (memory.length === 3) calculate()
+
+  if (memory.length) {
+    if (operators.some(i => memory.includes(i))) {
+      const index = memory.findIndex(j => operators.includes(j))
+      memory[index] = operator
+    } else {
+      memory.push(operator)
+    }
+  }
+
+  if (memory.length === 3) memory.shift()
+}
 
 Array.from(document.getElementsByClassName('operator')).forEach((el) => {
   el.addEventListener('click', () => {
-    verify()
-    // stored operator
     const operator = el.textContent
-
-    // push input to memory
-    if (cache.length) {
-      memory.push(cache.join(''))
-      cache.length = 0
-    }
-
-    // qualify to calculate if enough parts
-    if (memory.length === 3) calculate()
-
-    // handle replace or add operator to memory
-    if (memory.length) {      
-      if (operations.some(i => memory.includes(i))) {
-        const index = memory.findIndex(j => operations.includes(j))
-        memory[index] = operator
-      } else {
-        memory.push(operator)
-      }
-    }
-
-    // remove result if operator at the end
-    if (memory.length === 3) memory.shift()
-    verify()
+    handleOperators(operator)
   })
 })
 
+function handleEqual() {
+  if (cache.length) {
+    memory.push(cache.join(''))
+    cache.length = 0
+  }
+
+  if (memory.length === 2 && operators.some(i => memory.includes(i))) {
+    memory.push(memory[0])
+  }
+
+  if (memory.length === 2 && !operators.some(i => memory.includes(i))) {
+    memory.shift()
+  }
+
+  if (memory.length === 1 && lastEval.length) {
+    memory.push(lastEval[0])
+    memory.push(lastEval[1])
+  }
+
+  if (memory.length === 3) calculate()
+}
+
 Array.from(document.getElementsByClassName('equal')).forEach((el) => {
   el.addEventListener('click', () => {
-    if (cache.length) {
-      memory.push(cache.join(''))
-      cache.length = 0
-    }
-
-    if (memory.length === 2 && operations.some(i => memory.includes(i))) {
-      memory.push(memory[0])
-    }
-
-    if (memory.length === 2 && !operations.some(i => memory.includes(i))) {
-      memory.shift()
-    }
-
-    if (memory.length === 1 && lastEval.length) {
-      memory.push(lastEval[0])
-      memory.push(lastEval[1])
-    }
-
-    if (memory.length === 3) calculate()
-
-    verify()
+    handleEqual()
   })
 })
 
@@ -160,6 +171,25 @@ function setDisplay(string) {
   }
   display.textContent = '0'
 }
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.key)
+  if (integers.includes(e.key)) {
+    handleNumbers(e.key)
+  }
+  if (operators.includes(e.key)) {
+    handleOperators(e.key)
+  }
+  if (e.key === '.') {
+    handleDot(e.key)
+  }
+  if (e.key === '=' || e.key === 'Enter') {
+    handleEqual()
+  }
+  if (e.key === 'Escape') {
+    handleClear()
+  }
+})
 
 setDisplay()
 
